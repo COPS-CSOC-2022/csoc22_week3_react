c6ce4f40e1f537409447323f227e0d1424285f2f
nikhil <nikhil10july123@gmail.com>
2022-06-26 16:38:28 +0530

    Task 4 Completed

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,23 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
+
 import Link from 'next/link'
 import { useAuth } from '../context/auth'
-/**
- *
- * @todo Condtionally render login/register and Profile name in NavBar
- */
+import { displayError } from '../pages/_app';
+
+
+
+
 
 export default function Nav() {
-  const { logout, profileName, avatarImage } = useAuth()
+
+
+  const { logout, profileName, avatarImage, token } = useAuth()
+
+
+  function handleLogout()
+  {
+    logout() ;
+    displayError("Logged Out Successfully")
+  }
 
   return (
     <nav className='bg-blue-600'>
@@ -22,15 +31,19 @@ export default function Nav() {
             </Link>
           </li>
         </ul>
-        <ul className='flex'>
+
+
+        {(token == "null" || token == undefined) && <ul className='flex'>
           <li className='text-white mr-2'>
             <Link href='/login'>Login</Link>
           </li>
           <li className='text-white'>
             <Link href='/register'>Register</Link>
           </li>
-        </ul>
-        <div className='inline-block relative w-28'>
+        </ul>}
+
+
+        {(token != "null" && token != undefined) && <div className='inline-block relative w-28'>
           <div className='group inline-block relative'>
             <button className='bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center'>
               <img src={avatarImage} />
@@ -48,14 +61,14 @@ export default function Nav() {
                 <a
                   className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
                   href='#'
-                  onClick={logout}
+                  onClick={handleLogout}
                 >
                   Logout
                 </a>
               </li>
             </ul>
           </div>
-        </div>
+        </div>}
       </ul>
     </nav>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,45 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../context/auth'
 import Nav from '../components/Nav'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export function displaySuccess(message) {
+  toast.success(message);
+}
+
+export function displayError(message) {
+  toast.error(message);
+}
+
+export function displayWarning(message) {
+  toast.warning(message);
+}
+
+export function displayInfo(message) {
+  toast.info(message);
+}
+
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <AuthProvider>
       <Nav />
       <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-right"
+        theme="colored"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </AuthProvider>
   )
 }
