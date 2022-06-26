7cc2359d7b8195c28d484aaa57b5e546ea8f4c80
Archit Pattanaik <98776672+archit-2003@users.noreply.github.com>
2022-06-26 20:59:36 +0530

    Update TodoListItem.js

--- components/TodoListItem.js
+++ components/TodoListItem.js
@@ -1,55 +1,94 @@
-/* eslint-disable @next/next/no-img-element */
+import React, { useState} from 'react'
+import axios from '../utils/axios'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { useAuth } from "../context/auth"
+import { useRouter } from 'next/router'
 
-export default function TodoListItem() {
+
+export default function TodoListItem(props) {
   const editTask = (id) => {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Update the dom accordingly
-     */
+     document.getElementById("input-button-"+id).classList.remove("hideme");
+     document.getElementById("done-button-"+id).classList.remove("hideme");
+     document.getElementById("task-"+id).classList.add("hideme");
+     document.getElementById("task-actions-"+id).classList.add("hideme");
+    
   }
 
   const deleteTask = (id) => {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to delete the task to the backend server.
-     * @todo 2. Remove the task from the dom.
-     */
+    toast.info("Please wait...",{position: "top-center"});
+
+    const headersForApiRequest = {
+      headers: {Authorization: 'Token ' + token,}
+  }
+
+
+  axios.delete(
+    'todo/' + id + '/',headersForApiRequest,
+  ).then(function ({ data, status }) {
+       toast.success("Task has been deleted successfully.....",{position: "top-center"})
+    }).catch(function (err) {
+      toast.error("Couldn't delete the task ..Please try again....",{position: "top-center"})
+    }) 
+    
   }
 
   const updateTask = (id) => {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to update the task to the backend server.
-     * @todo 2. Update the task in the dom.
-     */
+    
+      if (updatetask===""){
+      toast.warn("Don't Leave any empty Spaces...Please enter a task...",{position: "top-center"});
+      return;
+  }
+   const dataForApiRequest={
+      title: updatetask
+   }
+
+   const headersForApiRequest = {
+    headers: {Authorization: 'Token ' + token,}
+}
+      axios.patch(
+        'todo/' + id + '/',dataForApiRequest,headersForApiRequest,
+      ).then(function ({ data, status }) {
+        document.getElementById("input-button-"+id).classList.add("hideme");
+        document.getElementById("done-button-"+id).classList.add("hideme");
+        document.getElementById("task-"+id).classList.remove("hideme");
+        document.getElementById("task-actions-"+id).classList.remove("hideme");
+        toast.success("Item in todo list Updated successfully...  ",{position: "top-center"})
+   }).catch(function (err) {
+       toast.warn("Not able to add task.Please try again.....",{position: "top-center"});
+   })
+    
   }
 
   return (
     <>
-      <li className='border flex border-gray-500 rounded px-2 py-2 justify-between items-center mb-2'>
+      <ToastContainer />
+      <li className='border flex border-gray-500 rounded px-2 py-2 justify-between items-center mb-2 '>
         <input
-          id='input-button-1'
+          id={`input-button-${props.id}`}
           type='text'
           className='hideme appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring  todo-edit-task-input'
           placeholder='Edit The Task'
+          value={updatetask}
+          onChange={(e) => setUpdatetask(e.target.value)}
         />
-        <div id='done-button-1' className='hideme'>
+        <div id={`done-button-${props.id}`} className='hideme'>
           <button
             className='bg-transparent hover:bg-gray-500 text-gray-700 text-sm  hover:text-white py-2 px-3 border border-gray-500 hover:border-transparent rounded todo-update-task'
             type='button'
-            onClick={updateTask(1)}
+            onClick={()=>updateTask(props.id)}
           >
             Done
           </button>
         </div>
-        <div id='task-1' className='todo-task  text-gray-600'>
-          Sample Task 1
+        <div id={`task-${props.id}`} className='todo-task  text-gray-600'>
+          {props.title}
         </div>
-        <span id='task-actions-1' className=''>
+        <span id={`task-actions-${props.id}`} className=''>
           <button
             style={{ marginRight: '5px' }}
             type='button'
-            onClick={editTask(1)}
+            onClick={()=>editTask(props.id)}
             className='bg-transparent hover:bg-yellow-500 hover:text-white border border-yellow-500 hover:border-transparent rounded px-2 py-2'
           >
             <img
@@ -62,7 +101,7 @@ export default function TodoListItem() {
           <button
             type='button'
             className='bg-transparent hover:bg-red-500 hover:text-white border border-red-500 hover:border-transparent rounded px-2 py-2'
-            onClick={deleteTask(1)}
+            onClick={()=>deleteTask(props.id)}
           >
             <img
               src='https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg'
