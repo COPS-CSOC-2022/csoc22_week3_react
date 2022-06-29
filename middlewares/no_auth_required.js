a92fbae5d336f754b421817f8a4b0475a79f581e
shrivardha <patil.shrivardhanc.cd.cse20@itbhu.ac.in>
2022-06-29 15:13:52 +0530

    React To-Do

--- middlewares/no_auth_required.js
+++ middlewares/no_auth_required.js
@@ -1,3 +1,21 @@
+
 /***
  * @todo Redirect the user to main page if token is present.
  */
+ import {useRouter} from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "../context/auth";
+ 
+function Check() {
+    const {
+      token
+    } = useAuth();
+    const route=useRouter();
+  useEffect(()=>{
+    if (token) {
+        route.push('/');
+      }
+  },[token])
+    
+  }
+ export const no_auth_required=()=>{Check()}
