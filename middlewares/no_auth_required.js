133010094b4051e376596d0f85525a973f283f07
peepo77 <96831986+peepo77@users.noreply.github.com>
2022-06-29 18:25:46 +0530

    Update no_auth_required.js

--- middlewares/no_auth_required.js
+++ middlewares/no_auth_required.js
@@ -1,3 +1,14 @@
-/***
- * @todo Redirect the user to main page if token is present.
- */
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "../context/auth";
+
+export default function noAuthRequired(){
+    const router = useRouter()
+    const { token } = useAuth()
+
+    useEffect(()=>{
+        if(token){
+            router.push('/')
+        }
+    },[token])
+}
