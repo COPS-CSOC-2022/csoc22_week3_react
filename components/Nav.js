83d9cc1e772866d44de9ad77dba3f8b791fe5a2f
Patil Shrivardhan Chandrakant <90125299+shrivardha@users.noreply.github.com>
2022-06-29 17:07:46 +0530

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -56,14 +56,14 @@ export default function Nav() {
               </svg>
             </button>
             {(token)&&<ul className='absolute hidden text-gray-700 pt-1 group-hover:block'>
-              <li className=''>
-                <a
-                  className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
+              <li className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap' onClick={logout}>
+                <Link
+                  //className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
                   href='/'
-                  onClick={logout}
+                  
                 >
                   Logout
-                </a>
+                </Link>
               </li>
             </ul>}
           </div>
