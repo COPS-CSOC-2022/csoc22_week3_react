e77bc79fc33b3751ff86a5993907b204927ed731
peepo77 <96831986+peepo77@users.noreply.github.com>
2022-06-29 18:23:33 +0530

    Update auth_required.js

--- middlewares/auth_required.js
+++ middlewares/auth_required.js
@@ -1,3 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "../context/auth"
+
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+export default function authRequired(){
+    
+    const router = useRouter()
+    const { token } = useAuth()
+    useEffect(()=>{
+        if(!token){
+            router.push('/login')
+        }
+    },[])
+}
