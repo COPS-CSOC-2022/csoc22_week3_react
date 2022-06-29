e63a5bf2407f64d024cdcb4bcffeb3395129a046
Krishna <krishnamadhwani2@gmail.com>
2022-06-29 22:12:18 +0530

    first_commit

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,44 @@ import { useEffect, useState } from 'react'
 import axios from '../utils/axios'
 import { useAuth } from '../context/auth'
 
+
 export default function Home() {
   const { token } = useAuth()
-  const [tasks, setTasks] = useState([])
+
 
   function getTasks() {
-    /***
-     * @todo Fetch the tasks created by the user.
-     * @todo Set the tasks state and display them in the using TodoListItem component
-     * The user token can be accessed from the context using useAuth() from /context/auth.js
-     */
+    axios
+     ( {
+       url:"https://todo-app-csoc.herokuapp.com/todo/",
+       headers: {
+         Authorization: 'Token ' + token,
+       },
+       method:"GET"
+     })
+     .then((response) => {
+        let data=response.data;
+        setTask(data);
+      })
+      .catch((error) => {
+       console.log(error)
+      })
+
   }
+  const [tasks, setTask] = useState([])
+  useEffect(()=>{
+    getTasks()
+  },[tasks])
 
   return (
     <div>
       <center>
-        <AddTask />
+        <AddTask getTasks={getTasks} />
         <ul className='flex-col mt-9 max-w-sm mb-3 '>
           <span className='inline-block bg-blue-600 py-1 mb-2 px-9 text-sm text-white font-bold rounded-full '>
             Available Tasks
           </span>
-          <TodoListItem />
+          {!tasks.length?<h1 className='dark:text-white text-gray-600 py-2'>No Todos</h1>:
+           tasks.map((task)=> (<TodoListItem key={task.id} {...task} />))}
         </ul>
       </center>
     </div>
